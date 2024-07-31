cfc2bfd22b52771a20988d10311966c527b25d52
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-07-31 19:07:23 +0500

    Basic of JavaScript and  variables and data types

---
+++ BASIC/index.js
@@ -0,0 +1,51 @@
+
+//basic output 
+
+//if you want to print some thing tin console
+console.log("hello");
+
+//if you want to print or show popup message  
+window.alert("popup");
+
+
+/*______________________________________*/
+
+
+//now of you want to comment some thing 
+//Double forward slashes (//) indicate that the subsequent characters
+// are a comment, up to and including the end-of-line characte
+
+/* In JavaScript, '/*......*/ /*is used to write multi-line comments.
+ Comments are text notes within your code that are ignored 
+ by the JavaScript interpreter, meaning they do not affect the 
+ execution of the code. */
+
+
+ // these comment is use for code readability 
+
+
+
+
+/*___________________________________________________________________________________________________*/
+
+
+ //now if you want change text content  or  print some text  in <h1> or <p> other html text field 
+/*
+
+ 1. document Object
+The document object represents the entire HTML document (or web page) that is loaded in the browser.
+ It is the entry point for accessing and manipulating the DOM (Document Object Model), which is a 
+ structured representation of the document's elements.
+
+2. getElementById("H1") Method
+getElementById: This is a method of the document object. It returns the first HTML element within 
+the document that matches the specified id attribute.
+"H1": This is the value of the id attribute used to identify the element. 
+The method will search the document for an element with id="H1".
+
+
+
+*/
+document.getElementById("H1").textContent="javascript Basic";
+
+
---
+++ Variable/index.js
@@ -0,0 +1,93 @@
+/*  
+In JavaScript, variables are used to store data values. You can think of a variable as a container that holds
+information that can be referenced and manipulated in your program. 
+*/
+
+/* var:
+
+Scope: Variables declared with var are function-scoped, meaning they are only
+accessible within the function in which they are declared. If declared outside 
+a function, they are globally scoped.
+
+
+*/
+
+/* 
+let:
+
+Scope: Variables declared with let are block-scoped, 
+meaning they are only accessible within the block (enclosed by {}) where they are defined.
+
+
+*/
+
+/* 
+const:
+Scope: Like let, const is block-scoped. 
+ */
+
+/* 
+Variable Naming Rules
+
+Letters, Digits, Underscores, and Dollar Signs: Variable names can include letters, digits, 
+underscores (_), and dollar signs ($). However, they cannot start with a digit.
+
+Case Sensitivity:
+Variable names are case-sensitive. myVariable and myvariable are considered different variables.
+
+Reserved Words: You cannot use JavaScript reserved words (like break, for, if, etc.) as variable names.
+ */
+
+/* 
+Variable Initialization
+
+let a = 5; // Declaration and initialization
+let b;     // Declaration
+b = 10;    // Initialization
+
+
+
+ */
+
+// Now code
+
+let x = 10; // Declaration and initialization
+x = 30;
+document.getElementById("Text").textContent = x;
+console.log(x);
+
+let a; // Declaration
+a = 20; // Initialization
+
+document.getElementById("Text2").textContent = a;
+console.log(a);
+
+//for example i want ot add student data
+
+let age = 20;
+let Name = "huzaifa";
+
+let fee = 20000;
+
+
+//witout ``` 
+console.log(` your ${age}` );
+console.log(typeof age ) // for type print 
+
+document.getElementById("Text2").textContent =
+  "your age" + age + "name is " + Name + "fee is " + fee;
+
+
+
+
+
+// data types 
+/*
+String:
+The string data type in JavaScript is used to represent textual data.
+ A string is a sequence of characters (letters, digits, symbols, whitespace, etc.)
+  enclosed within single quotes (' '), double quotes (" "), or backticks (\``).
+
+
+
+*/
