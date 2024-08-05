b58a1ecfd8c4574fedc50e4884de21ff15d9ec93
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-05 17:14:56 +0500

    type conversion

---
+++ typeconversin/index.js
@@ -0,0 +1,26 @@
+let value = 123;
+let result = String(value); // "123"
+
+
+
+let value2 = 123;
+let result2 = value.toString(); // "123"
+
+
+let value3 = "456";
+let result3 = Number(value); // 456
+
+
+let value4 = "789.01";
+let resultInt = parseInt(value); // 789
+let resultFloat = parseFloat(value); // 789.01
+
+let value5 = 1;
+let result5 = Boolean(value); // true
+
+
+
+console.log(result);
+console.log(result2);
+console.log(resultFloat);
+console.log(result5);
