7f0bfd9bc950b48353f532abbb3fe2cfb4cbb700
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-13 15:57:07 +0500

    ternaryopraters

---
+++ ternaryopraters/index.js
@@ -0,0 +1,26 @@
+// The ternary operator is a concise way to perform conditional operations
+// in programming. It’s essentially a shorthand for an if-else statement.
+
+
+let age;
+age=30;
+let message= age>=18 ? "you are 18+":"you are not 18+";
+
+console.log(message);
+
+
+let student;
+student=true;
+let message2= student ? "you are student ":"you are not student";
+
+console.log(message2);
+
+
+
+
+
+let purAmount;
+purAmount=100;
+let message3= purAmount <=1000 ? parseInt("10%"):parseInt("0%");
+
+console.log(message3);
