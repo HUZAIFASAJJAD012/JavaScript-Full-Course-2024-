24c9a632b6856bc3c7c866ac4d94c9ca0ea1afbc
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-14 18:31:43 +0500

    do while loop

--- loops/index.js
+++ loops/index.js
@@ -1,15 +1,26 @@
-// While loop = repeat some code while some condition  is true 
+// While loop = repeat some code while some condition  is true
+
+// let age = parseInt(window.prompt("enter your age", "enter your ae"));
+
+// while (age >= 18) {
+//   alert("you are 18+");
+
+//   age = 0;
+// }
 
 
 
-let age=parseInt(window.prompt("enter your age","enter your ae"));
 
 
-while(age>=18){
+// do while loop 
 
-alert("you are 18+");
+let age;
+do {
+   age = parseInt(window.prompt("enter your age", "enter your ae"));
+
+} while (age >= 18){
+
+  alert("you are 18+");
 
 
-age=0;
-}
-
+};
