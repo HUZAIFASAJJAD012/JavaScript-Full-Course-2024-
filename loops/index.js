51a2cd38610b8c25cc0866e05c9fb95d2beb646b
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-15 15:07:21 +0500

    for loop

--- loops/index.js
+++ loops/index.js
@@ -14,13 +14,50 @@
 
 // do while loop 
 
-let age;
-do {
-   age = parseInt(window.prompt("enter your age", "enter your ae"));
+// let age;
+// do {
+//    age = parseInt(window.prompt("enter your age", "enter your ae"));
 
-} while (age >= 18){
+// } while (age >= 18){
 
-  alert("you are 18+");
+//   alert("you are 18+");
 
 
-};
+// };
+// let usename;
+// let password;
+// let login=false;
+
+
+// while (!login) {
+// usename=window.prompt("enter your name","Enter your name");
+// password=window.prompt("enter your password","Enter your password");
+
+// if(usename=="huzaifa"&&password=="123"){
+
+// login=true;
+// alert("login successfuly  ");
+
+
+// }else{
+
+// alert("incorrect data  try agin ");
+
+
+// }
+
+  
+// }
+
+
+
+// *****************for loop********************
+
+
+for(let i=0 ;i<10;i++){
+
+
+console.log(i+1);
+
+
+}
