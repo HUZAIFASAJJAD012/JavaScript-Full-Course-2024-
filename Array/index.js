b1860b125890f2ef57a387d3713cdeb4442772fb
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-16 16:07:19 +0500

    add array

--- Array/index.js
+++ Array/index.js
@@ -1,15 +1,11 @@
-// array = a variable like   structure that can hold more than one values 
+// array = a variable like   structure that can hold more than one values
 
-
-
-
-let array=[16,52,43,35,62,43]
-
-
-for(let i=0;i<array.length;i++){
-
-array[i];
-console.log(array[i]);
-
-
+let array = [16, 52, 43, 35, 62, 43];
+array.push(637); // add value at the  end of arry 
+array.unshift(232); // add value it the start of the  array 
+array.shift();// remove value from the start of array
+for (let i = 0; i < array.length; i++) {
+  array[i];
+  console.log(array[i]);
 }
+   
