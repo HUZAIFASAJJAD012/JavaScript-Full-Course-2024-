a3597301aead0724f42719b49c0fdad36f99819d
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-16 14:26:06 +0500

    add array

---
+++ Array/index.js
@@ -0,0 +1,15 @@
+// array = a variable like   structure that can hold more than one values 
+
+
+
+
+let array=[16,52,43,35,62,43]
+
+
+for(let i=0;i<array.length;i++){
+
+array[i];
+console.log(array[i]);
+
+
+}
