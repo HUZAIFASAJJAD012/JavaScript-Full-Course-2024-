fa846d72ee0336d8d505d76e79a06fad5387d4e3
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-16 17:32:21 +0500

    spread operator

---
+++ spread operator/index.js
@@ -0,0 +1,17 @@
+// spread operator = ...array upacks the elements
+
+
+let array = [1,2,3,44,5,6];
+
+let max=Math.max(...array);
+let max1=[...array].join("_")
+console.log(max1);
+
+
+// add two arrays
+
+let array1 = [1,2,3,44,5,6];
+let array2 = [33,24,35,44,52,63];
+
+let add=[...array1,...array2];
+console.log(add);
