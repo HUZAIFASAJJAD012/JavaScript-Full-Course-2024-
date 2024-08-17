e0844f574822cd88cdc826ec42de9baf1a177b3d
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-17 15:01:32 +0500

    add two array

--- spread operator/index.js
+++ spread operator/index.js
@@ -10,8 +10,15 @@ console.log(max1);
 
 // add two arrays
 
-let array1 = [1,2,3,44,5,6];
-let array2 = [33,24,35,44,52,63];
+// let array1 = [1,2,3,44,5,6];
+// let array2 = [33,24,35,44,52,63];
 
-let add=[...array1,...array2];
-console.log(add);
+// let add=[...array1,...array2];
+// console.log(add);
+
+
+let array4 = [1,2,3,44,5,6];
+let array3 = ["33","24","35","44","52","63"];
+
+let add2=[...array3,parseInt(...array4)];
+console.log(add2);
