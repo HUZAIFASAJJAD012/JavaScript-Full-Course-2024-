80d3d8a0ca9f73f9b938d349caa79bc83849b253
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-22 17:32:42 +0500

    // map function

--- forEach/index.js
+++ forEach/index.js
@@ -1,7 +1,8 @@
 // forEach()
 /* The forEach() method is a common array method in JavaScript used to execute a provided function once for each array element. 
 It's often used for iterating over the elements of an array to perform actions, like logging them to the console, 
-modifying each element, or applying some logic */
+modifying each element, or applying some logic
+ but the forEach function change the arrauy value  */
 
 const numbers = [1, 2, 3, 4, 5];
 
@@ -28,7 +29,6 @@ animals.forEach((animal, index, array) => {
 const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 number.forEach(double);
 number.forEach(display);
-
 function double(element, index, array) {
   array[index] = element * 2;
 }
@@ -50,3 +50,41 @@ animal2.forEach(Display);
 function Display(element) {
   console.log(element);
 }
+
+
+
+
+
+
+// function submitFile() {
+//     // Prevent the default form submission
+//     event.preventDefault();
+
+//     // Get the file input element
+//     var fileInput = document.getElementById("file-upload");
+//     var file = fileInput.files[0];
+
+//     // Check if a file is selected
+//     if (file) {
+//         var reader = new FileReader();
+        
+//         // Define what happens when the file is successfully read
+//         reader.onload = function(event) {
+//             // Get the file content
+//             var fileContent = event.target.result;
+            
+//             // Split the content into lines (assuming it's a CSV file)
+//             var lines = fileContent.split('\n');
+            
+//             // Log each line in the console
+//             lines.forEach(function(line, index) {
+//                 console.log('Line ' + (index + 1) + ': ' + line);
+//             });
+//         };
+        
+//         // Read the file as text
+//         reader.readAsText(file);
+//     } else {
+//         console.log("No file selected");
+//     }
+//   }
--- map/index.js
+++ map/index.js
@@ -1,1 +1,27 @@
-//map =
+//map 
+
+/* accepts a callback and applies that function to each element of array the return a new array  */
+
+
+
+
+const number =[2,3,4,5,6,7,8,9];
+
+const multi= number;
+
+multi.map(sum);
+
+
+console.log(multi.map(sum));
+
+
+
+function sum(element){
+
+    return element *2;
+
+
+
+}
+
+ 
