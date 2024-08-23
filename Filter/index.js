7b7ee5cbc362b14b5b17f2044df02fbd4fc33cd4
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-24 02:04:30 +0500

    add filter method

---
+++ Filter/index.js
@@ -0,0 +1,19 @@
+//Filter = creates a new array by filtering out elements 
+
+
+  let number =[1,2,3,4,56,7,8,9];
+
+let   newnum =number.filter(odd);
+
+console.log(newnum);
+
+  function odd(element){
+
+
+return element%2!=0;
+
+  } 
+
+
+
+   
--- map/index.js
+++ map/index.js
@@ -1,40 +1,22 @@
-//map 
+//map
 
 /* accepts a callback and applies that function to each element of array the return a new array  */
 
+const number = [2, 3, 4, 5, 6, 7, 8, 9];
 
-
-
-const number =[2,3,4,5,6,7,8,9];
-
-const multi= number;
-
-multi.map(sum);
-
+const multi = number.map(sum);
 
 console.log(multi.map(sum));
 
-
-
-function sum(element){
-
-    return element *2;
-
-
-
+function sum(element) {
+  return element * 2;
 }
 
- 
+const students = ["ali", "huzaifa", "tanveer"];
 
-
-const  students= ['ali','huzaifa','tanveer'];
-
-const   uper=students.map(upperCase);
+const uper = students.map(upperCase);
 console.log(uper);
-function upperCase(element){
-
-
-return  element.toUpperCase();
-
-
+function upperCase(element) {
+  return element.toUpperCase();
 }
+
