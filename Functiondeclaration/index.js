273baa7f7d759e898d8b133f6c67faebed6accc7
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-24 21:03:39 +0500

    function declaration /  function declaration

--- Functiondeclaration/index.js
+++ Functiondeclaration/index.js
@@ -1,1 +1,21 @@
- /
+// function declaration = define  a reusabl block of code that perform a specific task
+
+function Hi() {
+  console.log("heloo");
+}
+
+// funcrion expressions = a way to define function as values or variables
+
+let fun = function Bye() {
+  console.log("bye bye");
+};
+
+fun();
+
+setTimeout(Hi, 3000);
+
+// OR
+
+setTimeout(function () {
+  console.log("helooo g");
+}, 4000);
