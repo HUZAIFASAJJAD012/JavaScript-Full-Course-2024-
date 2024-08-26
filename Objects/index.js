84cea11e49754f9f5bcb5d9d24fddcf8f5660dd0
Huzaifa Sajjad <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-26 03:14:29 -0700

    add object

--- Objects/index.js
+++ Objects/index.js
@@ -13,3 +13,21 @@ Hello:function(){console.log('heloo')}
 console.log(data.Name);
 console.log(data.Age);
 console.log(data.Hello());
+
+
+
+
+const data2 ={
+
+Name:`ali`,
+Age:24,
+Func:()=>{console.log(`you fee is 10,000000`)}
+
+
+
+
+
+}
+console.log(data2.Name);
+console.log(data2.Age);
+console.log(data2.Func);
