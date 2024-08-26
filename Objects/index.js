5ace71cd980cb219594616fd3e685b3df1a72c43
Huzaifa Sajjad <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-26 03:31:38 -0700

    add object

--- Objects/index.js
+++ Objects/index.js
@@ -1,33 +1,24 @@
 // Object =  a collection pf related properties  and/or methods can represent real world object (people,place porduct )
 
-
-
-const data={
-
-Name:`huzzaifa`, 
-Age:23,
-Hello:function(){console.log('heloo')}
-
+const data = {
+  Name: `huzzaifa`,
+  Age: 23,
+  Hello: function () {
+    console.log("heloo");
+  },
 };
 
 console.log(data.Name);
 console.log(data.Age);
 console.log(data.Hello());
 
-
-
-
-const data2 ={
-
-Name:`ali`,
-Age:24,
-Func:()=>{console.log(`you fee is 10,000000`)}
-
-
-
-
-
-}
+const data2 = {
+  Name: `ali`,
+  Age: 24,
+  Func: () => {
+    console.log(`you fee is 10,000000`);
+  },
+};
 console.log(data2.Name);
 console.log(data2.Age);
-console.log(data2.Func);
+console.log(data2.Func());
