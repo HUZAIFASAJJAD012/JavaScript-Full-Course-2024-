59a4ea09b07cce5f9a9d4d3344042903b36b97f3
Huzaifa Sajjad <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-08-30 05:47:06 -0700

    this keyword

--- this keyword/index.js
+++ this keyword/index.js
@@ -1,1 +1,25 @@
-// this keyword = ///
+// this keyword =  
+
+/* In JavaScript, this is a special keyword that refers to the context in which a function is executed.
+ The value of this can vary depending on how a function is called. Understanding this is crucial for working 
+ with objects and functions effectively. */
+
+
+const  data={
+
+Name:`huzaifa`,
+Age:23,
+PhoneNumber:'03335494995',
+Disply:function(){
+
+console.log(`you Name is ${this.Name} and age is ${this.Age} and  Phone Number is ${this.PhoneNumber} `)
+
+
+
+}
+
+}
+
+
+
+data.Disply();
