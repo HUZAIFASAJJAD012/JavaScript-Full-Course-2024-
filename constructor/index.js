c2b23d3593703adb1f5904738a7f3ae6dd616071
Huzaifa Sajjad <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-09-01 07:58:06 -0700

    add constructor

--- constructor/index.js
+++ constructor/index.js
@@ -1,1 +1,23 @@
-/// constructer
+//  constructor = 
+/* constructor is a special function used to create and initialize objects. Constructors are fundamental in object-oriented 
+programming and are typically used in conjunction with classes. */
+function Car(name ,model,price){
+
+    this.name=name;
+    this.model=model;
+    this.price=price;
+    
+    
+    
+    
+    
+    
+};
+
+
+const car1= new  Car('toyota',`toyoyta`,123412);
+
+
+
+
+console.log(`${car1.name}   ${car1.model}   Rs ${car1.price}`);
